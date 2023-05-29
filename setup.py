c3ffdd6cd94ea4d51ad1d0ef08ed22af031894bc
Trevor Bloomfield <bloomfieldtm@gmail.com>
2023-05-28 20:12:41 -0500

    update to 1.0.5

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='nounseed',
-    version='1.0.4',
+    version='1.0.5',
     description='A package for generating and storing project ideas',
     author='psibir',
     packages=['nounseed'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=[
         'argparse',
         'Pathlib'
